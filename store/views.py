bc761fdf751f395fcf2a3e92d32d4cbbb5a80af8
Prathik B <prathikreddy560@gmail.com>
2026-07-23 22:50:33 +0530

    Creating RESTful APIs Resources, serialization

--- store/views.py
+++ store/views.py
@@ -1,6 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.db.models.aggregates import Count, Min, Max, Avg, Sum
+from django.http import HttpResponse
 from store.models import Product,Customer, Order, OrderItem, Collection
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Product
+from .serializer import ProductSerializer,CollectionSerializer
 
 def say_hello(request):
     orders = Order.objects.filter(customer__id=1).aggregate(Count('id'))
@@ -10,3 +16,23 @@ def say_hello(request):
     return render(request, 'hello.html', {'name': 'Prathik', 'result': calculate})
 
 
+@api_view(['GET','POST'])
+def product_list(request):
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset, many=True,context={'request': request})
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+
+@api_view()
+def product_detail(request, id):
+    product = get_object_or_404(Product, pk=id)
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
+
+@api_view()
+def collection_detail(request, pk):
+    collection = get_object_or_404(Collection, pk)
+    serializer = CollectionSerializer(collection)
+    return Response(serializer.data)
