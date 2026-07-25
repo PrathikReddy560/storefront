7338c9645fd4000d8442cfc7c707f6fd08bab306
Prathik B <prathikreddy560@gmail.com>
2026-07-25 11:15:00 +0530

    Routers

--- store/views.py
+++ store/views.py
@@ -25,23 +25,20 @@ class ProductViewSet(ModelViewSet):
 
     def get_serializer_context(self):
             return {'request':self.request}
-    
-    def delete(self,request,pk):
-        product = get_object_or_404(Product, pk=pk)
-        if product.orderitems.count() > 0:
+
+    def destroy(self,request,*args,**kwargs):
+        if OrderItem.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error':'Product cannot be deleted cause it is associated with OrderItem'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
-            product.delete()
-            return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request,*args,**kwargs)    
     
 #-------------------------------------Collection-----------------------------------------------
 
 class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(products_count=Count('products')).all()
     serializer_class = CollectionSerializer
-    
-    def delete(self,request,pk):
-        collection = get_object_or_404(Collection, pk=pk)
-        if collection.product.count() > 0:
+
+    def destory(self,request,*args,**kwargs):
+        if Product.objects.filter(product_id=kwargs['pk']).count() > 0:
             return Response({'error':'Collection cannot be deleted cause it is associated with Product'},status=status.HTTP_405_METHOD_NOT_ALLOWED)
-        collection.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+        return super().destroy(request,*args,**kwargs)
+
