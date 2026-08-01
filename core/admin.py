ac980f4e4c02d0ee86b4778b77e02aab73182294
Prathik B <prathikreddy560@gmail.com>
2026-08-01 23:50:05 +0530

    Extending the User Model

--- core/admin.py
+++ core/admin.py
@@ -1,9 +1,21 @@
+
 from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from store.admin import ProductAdmin
 from store.models import Product
 from tags.models import Tag, TaggedItem
+from .models import User
 # Register your models here.
+
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2', 'email'),
+        }),
+    )
 
 class TagInline(GenericTabularInline):
   autocomplete_fields = ['tag']
--- store/models.py
+++ store/models.py
@@ -1,7 +1,8 @@
 from django.db import models
+from django.contrib import admin
 from django.core.validators import MinValueValidator, MaxValueValidator
 from uuid import uuid4
-
+from django.conf import settings
 class Promotion(models.Model):
     description = models.CharField(max_length=255)
     discount = models.FloatField()
@@ -53,12 +54,20 @@ class Customer(models.Model):
     birth_date = models.DateField(null=True)
     membership = models.CharField(
         max_length=1, choices=MEMBERSHIP_CHOICES, default=MEMBERSHIP_BRONZE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
+
+    @admin.display(ordering='user__first_name', description='First Name')
+    def first_name(self):
+        return self.user.first_name
+    @admin.display(ordering='user__last_name', description='Last Name')
+    def last_name(self):
+        return self.user.last_name
 
     def __str__(self):
-        return f'{self.first_name} {self.last_name}'
+        return f'{self.user.first_name} {self.user.last_name}'
 
     class Meta:
-        ordering = ['first_name', 'last_name']
+        ordering = ['user__first_name', 'user__last_name']
 class Order(models.Model):
     PAYMENT_STATUS_PENDING = 'P'
     PAYMENT_STATUS_COMPLETE = 'C'
